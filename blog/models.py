572ce32a239dcb09c7f1deef8a757e8fc2c305e2
Aditi Jain <jaditi930@gmail.com>
2022-10-24 15:33:03 +0530

    Created Blog Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from login.models import RegisterUser
+CATEGORY_CHOICES=[("1","Mental Health"),("2","Heart Disease"),("3","Covid19"),("4","Immunization")]
 # Create your models here.
+class blog(models.Model):
+    author_id=models.ForeignKey(RegisterUser,on_delete=models.CASCADE)
+    title=models.CharField(max_length=100)
+    image=models.ImageField()
+    category=models.CharField(max_length=100,choices=CATEGORY_CHOICES)
+    summary=models.CharField(max_length=200)
+    content=models.CharField(max_length=2000)
