74d30977b8af09745a8b4d3225a2872e4bdda88e
Aditi Jain <jaditi930@gmail.com>
2022-11-10 18:05:38 +0530

    Styling done

--- blog/templatetags/custom_tag.py
+++ blog/templatetags/custom_tag.py
@@ -2,6 +2,4 @@ from django import template
 register = template.Library()
 @register.filter(name="slice")
 def split1(value,l):
-  print(str(value))
-  print(str(value)[int(l)])
   return str(str(value)[int(l)])
